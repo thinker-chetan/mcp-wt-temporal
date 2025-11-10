e7845d402ff0b1fe86cb5962c27baad23ab0c0b9
thinker-chetan <chetan.sapkal@think41.com>
2025-11-10 15:42:29 +0530

    Initialize temporal worker

---
+++ temporal/worker.py
@@ -0,0 +1,31 @@
+import asyncio
+from temporalio.client import Client
+from temporalio.worker import Worker
+
+from temporal.activities import (
+    create_invoice_activity,
+    send_invoice_activity,
+    process_payment_activity,
+    complete_invoice_activity,
+)
+from temporal.workflow import InvoiceProcessingWorkflow
+
+
+async def main():
+    client = await Client.connect("localhost:7233")
+    worker = Worker(
+        client,
+        task_queue="invoice-task-queue",
+        workflows=[InvoiceProcessingWorkflow],
+        activities=[
+            create_invoice_activity,
+            send_invoice_activity,
+            process_payment_activity,
+            complete_invoice_activity,
+        ],
+    )
+    await worker.run()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
